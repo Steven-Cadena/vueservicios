660160bb8c4264d2ccaa6d8590d1094428dd1e89
Steven-Cadena <steven.cadena.giler@gmail.com>
2021-11-05 10:33:45 +0100

    servicios

---
+++ src/components/Coches.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Coches</h1>
+        <div>
+           <div class="card" style="width:18rem; float:left ; height: 20em" 
+           v-for="(coche,index) in coches" :key="index">
+               <img :src="coche.imagen" class="car-img-top" :title="coche.marca"/>
+               <div class="card-body">
+                   <h5 class="card-title"> {{coche.marca + " " + coche.modelo}}</h5>
+                   <p class="card-text">{{coche.conductor}}</p>
+               </div>
+           </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+//AQUI PODEMOS DECLARAR VARIABLES GLOBALES PARA TODO EL COMPONENTE 
+var urlapicoches ="https://apicochespaco.azurewebsites.net/";
+export default {
+    name:"Coches",
+    mounted(){
+        this.cargarCoches();
+    },
+    methods: {
+        cargarCoches(){
+            var request = "/webresources/coches";
+            var url = urlapicoches + request;
+            axios.get(url).then(res=>{
+                this.coches = res.data;
+                //console.log(this.coches);
+            })
+        }
+    },data(){
+        return{
+            coches:[],
+        }
+    }
+}
+</script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+    <div class="container-fluid">
+      <img src="./../assets/images/deadpool.png" style="width:50px"/>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarsExample03">
+        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <router-link to="/coches" class="nav-link" >Coches</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown03">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Menu",
+}
+</script>
